9bb5c19dc62fe010e357b02e2d453dfad3a69253
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2025-02-11 20:48:11 +0530

    Latest Commit

--- Main.jsx
+++ Main.jsx
@@ -22,12 +22,26 @@ export default function Main() {
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
                     name="ingredient"
+                    required
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            
+            {ingredients.length > 0 && <section>
+                
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                
+                {ingredients.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                    </div>
+                } 
+            </section>}
+            
         </main>
     )
 }
